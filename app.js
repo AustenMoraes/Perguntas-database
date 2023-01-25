5ab4adfb0913507f738d17a0f550347bdabeec3c
Austen Moraes <austenmoraes@gmail.com>
2023-01-25 18:27:51 -0300

    Commit Projeto Final

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/database');
-const perguntaModel = require('./database/Pergunta')
+const Pergunta = require('./database/Pergunta');
+const Resposta = require('./database/Resposta')
 // database
 
 connection
@@ -27,14 +28,58 @@ app.get('/perguntar',(req,res)=> {
 });
 
 app.get('/',(req,res)=>{
-  res.render('index');
+  Pergunta.findAll({ raw: true, order:[['id','DESC']]})
+  .then(perguntas => {
+    res.render('index', {
+      perguntas: perguntas
+    });
+  });
 });
 
 app.post('/salvarpergunta',(req,res)=>{
   var titulo = req.body.titulo;
   var descricao = req.body.descricao;
-  res.send('Recebido! Título:' + titulo + " " + "descricao:" + descricao );
+  Pergunta.create({
+    titulo: titulo,
+    descricao:descricao
+  }).then(()=>{
+    res.redirect('/')
+  })  
 });
 
+app.get('/pergunta/:id', (req,res)=>{
+  var id = req.params.id;
+  Pergunta.findOne({
+    where:{id:id}
+  }).then(pergunta => {
+    if(pergunta != undefined){
+
+      Resposta.findAll({
+        where: {perguntaId: pergunta.id},
+        order:[['id', 'DESC']]
+      }).then(respostas => {
+        res.render('pergunta',{
+          pergunta: pergunta,
+          respostas: respostas
+        });
+      })
+
+      
+    } else {
+      res.redirect('/');
+    }
+  })
+})
+
+app.post("/responder",(req, res) => {
+  var corpo = req.body.corpo;
+  var perguntaId = req.body.pergunta;
+  Resposta.create({
+      corpo: corpo,
+      perguntaId: perguntaId
+  }).then(() => {
+      res.redirect("/pergunta/"+perguntaId);
+  });
+});
 
 app.listen(8080,()=> {console.log('app rodando!')});
